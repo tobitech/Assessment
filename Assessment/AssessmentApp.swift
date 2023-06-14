9397e99754cd7753e728f071350af5d118986cc1
Tobi Omotayo <tobitech@ymail.com>
2023-06-14 12:00:27 +0100

    Initial Commit

---
+++ Assessment/AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentApp.swift
+//  Assessment
+//
+//  Created by Oluwatobi Omotayo on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
